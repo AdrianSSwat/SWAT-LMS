66743e5ee8d1388289d34e69f87b3a289dfce929
AdrianSSwat <adrian.solomon@gradstt.com>
2024-03-08 10:43:23 -0400

    Added my project

--- components/msword/page.js
+++ components/msword/page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from "next/link";
 
 const msword = () => {
   const posts = [
@@ -60,6 +61,21 @@ href: "https://www.youtube.com/watch?v=Hy2XzcW2mxc&t=1509s"
             }
         </div>
   </div>
+  <Link href='/Word' className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                Learn More
+                    <svg
+                        className="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <path d="M5 12h14" />
+                        <path d="M12 5l7 7-7 7" />
+                    </svg>
+                </Link>
 </section>
 
     </>
--- components/professional/page.js
+++ components/professional/page.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Link from "next/link";
+
 
 const professional = () => {
     const posts = [
@@ -28,40 +30,55 @@ const professional = () => {
     },
     ]
       return (
-        <>
-      <section className="bg-white dark:bg-gray-900">
-      <div className="container px-6 py-10 mx-auto">
-        <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
-        Professional Development
-        </h1>
-        <div className="mt-12 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
-                {
-                    posts.map((items, key) => (
-                        <div className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm relative" key={key}>
-                            <a href={items.href}>
-                                <img src={items.img} loading="lazy" alt={items.title}  className="object-cover object-center w-full h-64 rounded-lg lg:h-60" />
-                                <div className="flex items-center  pt-3 ml-4 mr-2">
-                                    <div className="flex-none w-10 h-10 rounded-full">
-                                        <img src={items.authorLogo} className="w-full h-full rounded-full" alt={items.authorName} />
+    <>
+        <section className="bg-white dark:bg-gray-900">
+            <div className="container px-6 py-10 mx-auto">
+                <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+                Professional Development
+                </h1>
+                <div className="mt-12 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
+                    {
+                        posts.map((items, key) => (
+                            <div className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm relative" key={key}>
+                                <a href={items.href}>
+                                    <img src={items.img} loading="lazy" alt={items.title}  className="object-cover object-center w-full h-64 rounded-lg lg:h-60" />
+                                    <div className="flex items-center  pt-3 ml-4 mr-2">
+                                        <div className="flex-none w-10 h-10 rounded-full">
+                                            <img src={items.authorLogo} className="w-full h-full rounded-full" alt={items.authorName} />
+                                        </div>
+                                        <div className="ml-3">
+                                            <span className="block dark:text-gray-100 text-gray-900">{items.authorName}</span>
+                                            <span className="block dark:text-gray-500 text-gray-400 text-sm">{items.date}</span>
+                                        </div>
                                     </div>
-                                    <div className="ml-3">
-                                        <span className="block dark:text-gray-100 text-gray-900">{items.authorName}</span>
-                                        <span className="block dark:text-gray-500 text-gray-400 text-sm">{items.date}</span>
+                                    <div className="pt-3 ml-4 mr-2 mb-3">
+                                        <h3 className="text-xl dark:text-gray-100 text-gray-900">
+                                            {items.title}
+                                        </h3>
                                     </div>
-                                </div>
-                                <div className="pt-3 ml-4 mr-2 mb-3">
-                                    <h3 className="text-xl dark:text-gray-100 text-gray-900">
-                                        {items.title}
-                                    </h3>
-                                </div>
-                            </a>
-                        </div>
-                    ))
-                }
+                                </a>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
-      </div>
-    </section>
-        </>
+                <Link href='/Professional-Development' className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                Learn More
+                    <svg
+                        className="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <path d="M5 12h14" />
+                        <path d="M12 5l7 7-7 7" />
+                    </svg>
+                </Link>
+        </section>
+    </>
   )
 }
 
